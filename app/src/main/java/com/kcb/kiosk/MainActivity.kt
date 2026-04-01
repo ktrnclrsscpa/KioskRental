8979aa0f8e129986345c98e7dcc3c111899767be
ktrnclrsscpa <kclarissecpa@gmail.com>
2026-04-01 20:35:23 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/kcb/kiosk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kcb.kiosk
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "App working!", Toast.LENGTH_LONG).show()
+    }
+}
