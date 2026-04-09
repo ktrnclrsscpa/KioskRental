850f592814d71d8915e6df933e23bd9a6f37c8e0
ktrnclrsscpa <kclarissecpa@gmail.com>
2026-04-09 21:15:40 +0800

    h

--- app/src/main/java/com/kcb/kiosk/SupabaseClient.kt
+++ app/src/main/java/com/kcb/kiosk/SupabaseClient.kt
@@ -2,8 +2,7 @@ package com.kcb.kiosk
 
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.from
-import io.github.jan.supabase.postgrest.query.filter.eq
+import io.github.jan.supabase.postgrest.postgrest
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -14,7 +13,6 @@ data class PinRes(
 )
 
 class SupabaseClient {
-    // Siguraduhin na tama ang URL at Key mo dito
     private val client = createSupabaseClient(
         supabaseUrl = "https://qbrjcrnjchbdyseeuwif.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFicmljcm5qY2hiZHlzZWV1d2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMDU0NDUsImV4cCI6MjA4OTc4MTQ0NX0.5sJqi3fZc4VIFQAIw1QptHt7MlGdnkn5SVxYdRu4f7Q"
@@ -24,15 +22,15 @@ class SupabaseClient {
 
     companion object {
         @Volatile
-        private var instance: com.kcb.kiosk.SupabaseClient? = null
+        private var instance: SupabaseClient? = null
         fun getInstance() = instance ?: synchronized(this) {
-            instance ?: com.kcb.kiosk.SupabaseClient().also { instance = it }
+            instance ?: SupabaseClient().also { instance = it }
         }
     }
 
     suspend fun validatePin(pinValue: String): PinRes {
         return try {
-            client.from("credits").select {
+            client.postgrest.from("credits").select {
                 filter {
                     eq("pin", pinValue)
                 }
@@ -44,7 +42,7 @@ class SupabaseClient {
 
     suspend fun getCurrentRemainingSeconds(pinValue: String): Long {
         return try {
-            val res = client.from("credits").select {
+            val res = client.postgrest.from("credits").select {
                 filter {
                     eq("pin", pinValue)
                 }
@@ -57,7 +55,7 @@ class SupabaseClient {
 
     suspend fun updatePinTime(pinValue: String, newSeconds: Long, amount: Double, isExtension: Boolean): Boolean {
         return try {
-            client.from("credits").update({
+            client.postgrest.from("credits").update({
                 set("seconds_left", newSeconds)
                 set("amount", amount)
                 set("is_extension", isExtension)
